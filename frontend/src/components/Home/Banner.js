6397d08fbf6629b7a081b8136870da3180fb9cd1
Moisés Man <mcarlosman@gmail.com>
2022-12-14 15:25:28 +0000

    prettier

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -2,13 +2,13 @@ import React, {useState} from "react";
 import logo from "../../imgs/logo.png";
 
 const Banner = (props) => {
-  const [searchText, setSearchText]=useState("")
+  const [searchText, setSearchText] = useState("")
 
   const handleChange = (e) => {
     const text = e.target.value
     setSearchText(text)
 
-    if(text.length>=3){
+    if(text.length >= 3) {
       props.onSearchByTitle(text)
     }
   }
@@ -20,7 +20,7 @@ const Banner = (props) => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
-          <input type="text" id="search-box" value={searchText} onChange={handleChange}/>
+          <input type="text" id="search-box" value={searchText} onChange={handleChange} />
           <span> the cool stuff.</span>
         </div>
       </div>
