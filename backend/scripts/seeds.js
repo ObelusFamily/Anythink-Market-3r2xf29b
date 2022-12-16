8fb2ac477cda4d6b9525acddd6f7791bdc1bbb8e
Moisés Man <mcarlosman@gmail.com>
2022-12-16 21:22:35 +0000

    data seed script

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -4,21 +4,28 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 require("../models/User");
 require("../models/Item");
+require("../models/Comment");
 
 var User = mongoose.model("User");
 var Item = mongoose.model("Item");
+var Comment = mongoose.model("Comment");
 
 
 (async () => {
     await Item.deleteMany({});
     await User.deleteMany({});
 
-    const user = new User({username: "test1", email:"test1@mail.com"});
-    user.setPassword("test");
-    await user.save(); 
+    for (let i = 0; i < 100; i++) {
+        const user = new User({username: `test${i}`, email: `test${i}@mail.com`});
+        user.setPassword("test");
+        await user.save(); 
 
-    const item1 = new Item({slug: "item1", title: "item1 title", seller: user});
-    await item1.save(); 
+        const comment1 = new Comment({body: `some comment ${i}`, seller: user});
+        await comment1.save()
 
+        const item1 = new Item({slug: `item${i}`, title: `item${i} title`, description: "", seller: user, comments: [comment1]});
+        await item1.save(); 
+
+    }
     await mongoose.disconnect();
 })();
